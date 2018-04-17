ff95f601beb20dd72b0bb5b4847096a5aab8ed09
Aden Collinge <aden.collinge@gmail.com>
2018-04-17 00:08:15 +0000

    project functional

--- download_avatars.js
+++ download_avatars.js
@@ -3,20 +3,27 @@ const token = require('./secrets.js');
 const fs = require('fs');
 const path = require('path');
 
+const owner = process.argv[2];
+const repo = process.argv[3];
 
 const mkdirSync = function (dirPath) {
-  try {
+ try {
     fs.mkdirSync(dirPath)
   } catch (err) {
-    if (err.code !== 'EEXIST') throw err
+   if (err.code !== 'EEXIST') throw err
   }
 }
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
+  if(!repoOwner || !repoName) {
+    console.log("ERROR: Provide repo owner and name ");
+    process.exit(1);
+  }
+
   let options = {
-    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    url: `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`,
     headers: {
       'User-Agent': 'request',
       'Authorization': `token ${token.GITHUB_TOKEN}`
@@ -28,9 +35,6 @@ function getRepoContributors(repoOwner, repoName, cb) {
     cb(err, bodyObject);
   });
 }
-
-
-getRepoContributors("jquery", "jquery", getAvatarUrl);
 
 function getAvatarUrl(err, result){
   if (err) {console.log(err, "ERROR")};
@@ -49,3 +53,5 @@ function downloadImageByURL(url, filePath) {
   request(options, (err, res, body) => {
   }).pipe(fs.createWriteStream(filePath));
 }
+
+getRepoContributors(owner, repo, getAvatarUrl);
